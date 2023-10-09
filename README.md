6dc078ec61a1a9995d778776722c90fd624213c3
ahmedrazapk <ahmedrazapk>
2023-10-10 04:29:09 +0500

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Raja
+
+r
+
+#### License
+
+MIT
---
+++ raja/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Raja",
+			"color": "grey",
+			"icon": "octicon octicon-file-directory",
+			"type": "module",
+			"label": _("Raja")
+		}
+	]
